0299fcd3aabe00a23304c0105cddc2ea89dc6015
Simone Masiero <simone@duiker101.net>
2020-04-18 22:44:35 +0300

    better example

--- example/index.tsx
+++ example/index.tsx
@@ -1,25 +1,37 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
-import {ShadowPicker, ShadowPickerParams} from "../src";
+import {ShadowPicker} from "../src";
 
 const App = () => {
+	const [custom, setCustom] = useState(false);
 	const [shadow, setShadow] = useState("0px 0px 0px");
-	// const [params, setParams] = useState<ShadowPickerParams>({
-	// 	// offset: {x: "20px", y: "20px"},
-	//     blur: "20px"
-	// });
 
 	return (
-		<div style={{boxShadow: shadow + " #0005"}}>
-			<ShadowPicker
-				className={"custom-picker"}
-				// params={params}
-				onChange={(value, params) => {
-					setShadow(value);
-					// setParams(params);
-				}}
-			/>
+		<div className={"wrapper"}>
+			<div>
+				<label>
+					Custom theme
+					<input
+						type={"checkbox"}
+						checked={custom}
+						onChange={() => setCustom(!custom)}
+					/>
+				</label>
+				<div>
+					<ShadowPicker
+						className={custom && "custom-picker"}
+						onChange={(value, params) => {
+							setShadow(value);
+						}}
+					/>
+				</div>
+			</div>
+			<div className={"example-wrapper"}>
+				<div
+					className={"example"}
+					style={{boxShadow: shadow + " #0005"}}></div>
+			</div>
 		</div>
 	);
 };
