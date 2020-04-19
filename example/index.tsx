094a0563ead854fa376edc2b7a7135c7995f3f75
Simone Masiero <simone@duiker101.net>
2020-04-19 03:02:51 +0300

    trying to fix the imports

--- example/index.tsx
+++ example/index.tsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import ReactDOM from "react-dom";
-import {ShadowPicker, ShadowPickerParams} from "react-shadow-picker";
+import {ShadowPicker, ShadowPickerParams} from "../lib/src";
 import "./main.css";
 
 const App = () => {
